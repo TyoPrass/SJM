cb652767afa87cc26b46f76368fd0f07cc858f81
TyoPrass <220441100072@student.trunojoyo.ac.id>
2025-03-18 20:40:06 +0700

    Tampilan disini

---
+++ View/dashboard/view.php
@@ -0,0 +1,41 @@
+<?php
+
+include '../navbar.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard View</title>
+    <link rel="shortcut icon" href="../assets/images/favicon.ico">
+
+        <!-- third party css -->
+        <link href="../../assets/css/vendor/jquery-jvectormap-1.2.2.css" rel="stylesheet" type="text/css" />
+        <!-- third party css end -->
+
+        <!-- App css -->
+        <link href="../../assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+        <link href="../../assets/css/app.min.css" rel="stylesheet" type="text/css" id="app-style"/>
+
+</head>
+<body>
+    <div class="content">
+        <h1>Welcome to the Dashboard</h1>
+        <p>This is the main content area of the dashboard.</p>
+    </div>
+
+
+    <script src="../../assets/js/vendor.min.js"></script>
+        <script src="../assets/js/app.min.js"></script>
+
+        <!-- third party js -->
+        <script src="../../assets/js/vendor/apexcharts.min.js"></script>
+        <script src="../../assets/js/vendor/jquery-jvectormap-1.2.2.min.js"></script>
+        <script src="../../assets/js/vendor/jquery-jvectormap-world-mill-en.js"></script>
+        <!-- third party js ends -->
+
+        <!-- demo app -->
+        <script src="../../assets/js/pages/demo.dashboard.js"></script>
+</body>
+</html>
